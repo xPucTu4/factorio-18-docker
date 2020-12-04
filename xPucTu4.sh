2b0ce239a1ed086d70192ebcdd67434733039326
xPucTu4 <hristo+git@fenix.international>
2020-12-05 01:26:28 +0200

    Updated to keep up with 1.x experimental versions

--- xPucTu4.sh
+++ xPucTu4.sh
@@ -19,7 +19,7 @@ function checkNewVersion()
     TFN=$(mktemp)
     wget -O $TFN -o /dev/null "https://factorio.com/download"
 #    cat $TFN | grep -E "^(\s{3,})Exp\S* - [0-9.]{3,}$" | cut -f2 -d "-" | tr -d " " > /factorio/lkv
-    echo $(cat $TFN | grep -E "\"/get-download/[0-9.]{5,}/headless" | cut -f2 -d '=' | tr -d '"' | cut -f3 -d '/') > /factorio/lkv
+    echo $(cat $TFN | grep '<!-- experimental -->' -A 10 | grep -E "\"/get-download/[0-9.]{5,}/headless" | cut -f2 -d '=' | tr -d '"' | cut -f3 -d '/') > /factorio/lkv
     rm $TFN
     echo "Version check completed"
 }
